2c80da313dd69c1ebbb108bb02c6122e1459787f
Jhonatas Santos <jhonatassantos@MacBook-Pro-de-Jhonatas.local>
2020-09-26 22:10:23 -0300

    feat: added cors in backend and added axios config in web project

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -1,9 +1,11 @@
-import express from 'express';
-import routes from './routes';
+import express from 'express'
+import cors from 'cors'
+import routes from './routes'
 
-const app = express();
+const app = express()
 
-app.use(express.json());
-app.use(routes);
+app.use(cors())
+app.use(express.json())
+app.use(routes)
 
-export default app;
+export default app
