a160b347d194be1c6f72f5afb114bb936daffb4c
Jhonatas Santos <jhonatassantos@MacBook-Pro-de-Jhonatas.local>
2020-09-26 21:23:06 -0300

    style added home style and input style

--- packages/web/src/pages/Home/styles.ts
+++ packages/web/src/pages/Home/styles.ts
@@ -1,12 +1,68 @@
 import styled from 'styled-components'
+import { shade } from 'polished'
 
 export const Container = styled.div`
-  width: 100vw;
   height: 100vh;
-  justify-content: center;
+
+  display: flex;
+  align-items: stretch;
+`;
+
+export const Content = styled.div`
+  flex: 1;
+  display: flex;
   align-items: center;
-`
-export const Text = styled.h1`
-  font-size: 20px;
-  color: #000;
-`
+  place-content: center;
+
+  form {
+    margin: 80px 0;
+    width: 350px;
+    text-align: center;
+
+    h1 {
+      margin-bottom: 24px;
+      font-weight: 700;
+    }
+
+    button {
+      background: #333;
+      height: 56px;
+      border-radius: 10px;
+      border: 0;
+      padding: 0 16px;
+      color: #ffff00;
+      width: 100%;
+      font-weight: 500;
+      margin-top: 18px;
+      transition: background-color 0.2s;
+
+      &:hover {
+        background: ${shade(0.2, '#333')}
+      }
+    }
+  }
+`;
+
+export const Background = styled.div`
+  width: 800px;
+  background-color: #ffff00;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  place-content: center;
+
+  img {
+    width: 280px;
+    height: 280px;
+  }
+
+  h1 {
+    margin-top: 16px;
+    font-weight: 700;
+  }
+
+  h2 {
+    margin-top: 14px;
+  }
+`;
