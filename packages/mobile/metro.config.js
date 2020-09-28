231f2e0de276f9d8a599be7bbb5edde9421110e7
Jhonatas Santos <jhonatassantos@MacBook-Pro-de-Jhonatas.local>
2020-09-28 15:01:41 -0300

    feat: integrated api in mobile project

--- packages/mobile/metro.config.js
+++ packages/mobile/metro.config.js
@@ -5,17 +5,22 @@
  * @format
  */
 
-const path = require('path')
+const path = require('path');
 
 module.exports = {
   projectRoot: path.resolve(__dirname, '.'),
-  watchFolders: [path.resolve(__dirname, '../../node_modules')],
+
+  watchFolders: [
+    path.resolve(__dirname, '../../node_modules'),
+    path.resolve(__dirname, '../axios-config')
+  ],
+
   transformer: {
     getTransformOptions: async () => ({
       transform: {
         experimentalImportSupport: false,
-        inlineRequires: false
-      }
-    })
-  }
-}
+        inlineRequires: false,
+      },
+    }),
+  },
+};
