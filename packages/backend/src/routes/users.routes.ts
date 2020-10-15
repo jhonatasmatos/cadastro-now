906a76427accb45d21c85b0f735bbe8d14f72061
Micael Levi <mllc@icomp.ufam.edu.br>
2020-10-15 16:25:48 -0400

    feat: added API get user (by id)
    Closes #11

--- packages/backend/src/routes/users.routes.ts
+++ packages/backend/src/routes/users.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express'
 import UserController from '../controllers/UserController'
 
-const router = Router();
+const router = Router()
 
-router.post("/", UserController.create)
-router.get("/", UserController.index)
+router.post('/', UserController.create)
+router.get('/', UserController.index)
+router.get('/:userId', UserController.view)
 
 export default router
